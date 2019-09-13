42596c1066eec21830a6d89462bb6cf76ab61905
samrays <samchuksy@gmail.com>
2019-09-13 22:55:13 +0100

    Adnaira mobile library

---
+++ adnaira_mob/src/main/java/com/adnaira/adnaira_mob/Server_ad.java
@@ -0,0 +1,10 @@
+package com.adnaira.adnaira_mob;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface Server_ad {
+    @GET("{id}/{ip}")
+    Call<AddInfo> getAddInfo(@Path("id") String token, @Path("ip") String ipAdd);
+}
